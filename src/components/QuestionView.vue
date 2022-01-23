212afd4155b654e25d05bba2d24653538b70e332
Michel Saremi <michel.saremi@outlook.dk>
2022-01-23 09:50:11 +0100

    PR: Finished QuestionView draft

--- src/components/QuestionView.vue
+++ src/components/QuestionView.vue
@@ -3,44 +3,47 @@ import {reactive, ref} from 'vue'
 import App from '../App.vue'
 
 const Answers = reactive(["HelloMeoo", "Jhonny","mahh","boosboos"]) 
-const question = "What is the cutest neckname?"
+const question = "What is the cutest nickname?"
 
-const MultipleChoice= ref(false)
-const TrueOrFalse= ref(false)
-
-const Questiontype = props.QuestionType
 
 const props = defineProps({
   QuestionType: String
 })
 
-console.log(props.QuestionType)
+console.log("from child " +props.QuestionType)
 
 </script>
 
 <template>
-
-  <div v-show="Questiontype=='MultipleChoice'">
+  
+  <div v-if="QuestionType=='MultipleChoice'">
     <h2>The is the QuestionView</h2>
     <h2>{{question}}</h2>
-    <ul> 
+    <ol> 
       <li v-for="(answer, i) in Answers" :key="i" >
          <button type="button" > {{ answer }}</button><br><br>
      </li>
-    </ul>
+    </ol>
   </div>
 
-  <div v-show="Questiontype=='TrueOrFalse'">
+  <div v-if="QuestionType=='TrueOrFalse'">
     <h2>The is the QuestionView</h2>
     <h2>{{question}}</h2>
          <button type="button">True</button><br><br>
          <button type="button">False</button><br><br>
   </div>
-  
+
+
 </template>
 
 <style scoped>
 h2 {
   color: blue;
 }
+ol {
+  width: 100px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 </style>
