bda46a58641283183d3d7220c35350d90150b2ef
oliver.hauck <oliver.hauck@de.experis.com>
2022-01-23 17:34:33 +0100

    Feat: StartView builds URL for API call based on user input

--- src/components/StartView.vue
+++ src/components/StartView.vue
@@ -1,30 +1,44 @@
 <script setup>
 import {reactive, ref} from 'vue';
 const username = ref('') 
-const Qnumber = ref('')
+const Qnumber = ref(10)
+const questions = reactive([]); 
 
 const emit = defineEmits(['toggle-visibilityStart'])
 
-const onSubmit = () => {
-    emit('toggle-visibilityStart') 
-}
-
-    const difficulties = ref(["easy", "medium", "hard"]);
-
-    const questions = reactive([]);
-    fetch("https://opentdb.com/api.php?amount=10")
-      .then(response => response.json())
-      .then(result => { 
-        for (const iterator of result.results) {
-          questions.push(iterator);
+    const onSubmit = () => {
+        if(Qnumber.value <= 10 && Qnumber.value >= 1) {
+          emit('toggle-visibilityStart');
+          let url = `https://opentdb.com/api.php?amount=${Qnumber.value}`;
+          if(selectedCategoryId.value != "") {
+            url += `&category=${selectedCategoryId.value}`;
+          }
+          if(selectedDifficulty.value != "") {
+            url += `&difficulty=${selectedDifficulty.value}`;
+          }
+          console.log("url",url);
+          fetch("https://opentdb.com/api.php?amount=10&category=21&difficulty=easy")
+            .then(response => response.json())
+            .then(result => { 
+              for (const iterator of result.results) {
+                questions.push(iterator);
+              }
+            })
         }
-      })
+        else {
+          alert("Number of questions must be between 1 and 10...");
+        }
+        
+    }
+
+    const difficulties = ["easy", "medium", "hard"];
+    const selectedDifficulty = ref("");
+    const selectedCategoryId = ref("");
 
     const categories = reactive([]);
     fetch("https://opentdb.com/api_category.php")
     .then(response => response.json())
     .then(result => { 
-      console.log("result", result.trivia_categories)
       for (const iterator of result.trivia_categories) {
            categories.push(iterator);
       }
@@ -36,24 +50,33 @@ const onSubmit = () => {
 
 <template>
   <div>
-  <h2>The is the StartView</h2><br>
-  <label>Please type in your Username</label><br>
-  <input type="text" placeholder="Type in your username!" v-model="username"><br><br>
+    <h2>This is the StartView</h2><br>
+    <label>Please type in your Username</label><br>
+    <input type="text" placeholder="Type in your username...!" v-model="username"><br><br>
+    
+    <label>Please select your difficulty</label><br>
+    <select v-model="selectedDifficulty">
+      <option 
+        v-for="difficulty in difficulties" 
+        :key="difficulty">{{difficulty}}</option> 
+    </select><br><br>
+    
+    <label>Please select number of questions</label><br />
+    <input 
+      type="number" min="1" max="10" 
+      placeholder="Type in a number between 1 and 10" v-model="Qnumber" />
   
-  <label>Please select your difficulty</label><br>
-  <select v-model="difficulties">
-    <option v-for="difficulty in difficulties" :key="difficulty" >{{difficulty}}</option> 
-  </select><br><br>
-  
-  <label>Please select number of questions</label><br>
-  <input type="number" min="1" max="10" placeholder="Type in a number between 1 and 10" v-model="Qnumber"><br><br>
-  
-  <label>Please select your category</label><br>
-  <select v-model="categories">
-    <option v-for="category in categories" :key="category.id" >{{category.name}}</option> 
-  </select><br><br>
+      <br><br>
+    
+    <label>Please select your category</label><br>
+    <select v-model="selectedCategoryId">
+      <option 
+        v-for="category in categories" 
+        :key="category.id"
+        :value="category.id">{{category.name}}</option> 
+    </select><br><br>
 
-  <button @click="onSubmit">Start quiz</button>
+    <button @click="onSubmit">Start quiz</button>
   </div>
 </template>
 
